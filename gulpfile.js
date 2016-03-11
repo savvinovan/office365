f264c7c4182bbe460c94b5473e1512ff5a550652
Savvinov Aleksandr <savvinovan@gmail.com>
2016-03-11 18:15:51 +0900

    second commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,31 @@
 var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
-var sass        = require('gulp-sass');
+var stylus        = require('gulp-stylus');
+var gulpJade    = require('gulp-jade');
 gulp.task('browser-sync', function(){
     browserSync.init({
         server: {
             baseDir: "./app"
         }
     });
-    gulp.watch("app/*.html").on('change', browserSync.reload);
-    gulp.watch("app/scss/*.scss", ['sass']);
+    gulp.watch("./app/stylus/*.styl", ['stylus']);
+    gulp.watch("./app/jade/*.jade", ['jade']);
 })
 
-gulp.task('default', ['sass','browser-sync'])
+gulp.task('default', ['stylus','jade','browser-sync'])
 
-gulp.task('sass', function() {
-    return gulp.src("app/scss/*.scss")
-        .pipe(sass())
-        .pipe(gulp.dest("app/css"))
-        .pipe(browserSync.stream);
+gulp.task('stylus', function () {
+  return gulp.src('./app/stylus/*.styl')
+    .pipe(stylus())
+    .pipe(gulp.dest('./app/css'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('jade', function(){
+    return gulp.src('./app/jade/*.jade')
+        .pipe(gulpJade({
+            pretty: true
+        }))
+        .pipe(gulp.dest('./app'))
+        .pipe(browserSync.stream());
 });
