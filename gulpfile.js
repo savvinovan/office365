11631c0fc77efccb24079907dc5cf91c6f07e5c5
Savvinov Aleksandr <savvinovan@gmail.com>
2016-03-14 17:34:19 +0900

    Added magic scroll

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
 var stylus        = require('gulp-stylus');
-var gulpJade    = require('gulp-jade');
+
 gulp.task('browser-sync', function(){
     browserSync.init({
         server: {
@@ -9,23 +9,14 @@ gulp.task('browser-sync', function(){
         }
     });
     gulp.watch("./app/stylus/*.styl", ['stylus']);
-    gulp.watch("./app/jade/*.jade", ['jade']);
+    gulp.watch("./app/*.html", browserSync.reload)
 })
 
-gulp.task('default', ['stylus','jade','browser-sync'])
+gulp.task('default', ['stylus','browser-sync'])
 
 gulp.task('stylus', function () {
   return gulp.src('./app/stylus/*.styl')
     .pipe(stylus())
     .pipe(gulp.dest('./app/css'))
     .pipe(browserSync.stream());
-});
-
-gulp.task('jade', function(){
-    return gulp.src('./app/jade/*.jade')
-        .pipe(gulpJade({
-            pretty: true
-        }))
-        .pipe(gulp.dest('./app'))
-        .pipe(browserSync.stream());
 });
